3637f8e81ee8671ab1531b026c7dc723c5124e60
sky-big <sky_big@yeah.net>
2019-10-29 05:27:30 -0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f gitlab-namespace.yaml
-kubectl apply -f gitlab-postgresql.yaml
-kubectl apply -f gitlab-redis.yaml
-kubectl apply -f gitlab.yaml
+kubectl apply -f ./gitlab/gitlab-namespace.yaml
+kubectl apply -f ./gitlab/gitlab-postgresql.yaml
+kubectl apply -f ./gitlab/gitlab-redis.yaml
+kubectl apply -f ./gitlab/gitlab.yaml
