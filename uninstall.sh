72ed103a0f2fff313589703cbe4a0400892ba6f4
sky-big <sky_big@yeah.net>
2019-10-30 00:01:39 -0700

    use statefulset and pvc

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl delete -f ./gitlab/gitlab-namespace.yaml
-kubectl delete -f ./gitlab/gitlab-postgresql.yaml
-kubectl delete -f ./gitlab/gitlab-redis.yaml
 kubectl delete -f ./gitlab/gitlab.yaml
+kubectl delete -f ./gitlab/gitlab-redis.yaml
+kubectl delete -f ./gitlab/gitlab-postgresql.yaml
+kubectl delete -f ./gitlab/gitlab-namespace.yaml
